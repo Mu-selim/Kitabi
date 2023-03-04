69d23aa824bb6c2e62fde59f9e9191822441fa2f
Muhammad Selim <muhammadselim@duck.com>
2023-03-04 12:23:36 +0200

    menu customization

--- src/components/navbar/leftNav/navList/NavList.js
+++ src/components/navbar/leftNav/navList/NavList.js
@@ -2,16 +2,24 @@ import { Link } from "react-router-dom";
 import Discount from "../../../../utils/icons/discount/Discount";
 import Upload from "../../../../utils/icons/upload/Upload";
 import "./NavList.scss";
+import { useContext } from "react";
+import { MenuContext } from "../../../../hooks/contexts/useMenuContext";
 
 const NavList = () => {
+  const {setMenu} = useContext(MenuContext);
+
+  const clickHandle = () => {
+    setMenu(null);
+  }
+
   return (
     <ul className="nav-list">
-      <li>
+      <li onClick={clickHandle}>
         <Link className="nav-links" to="/books">
           الكتب
         </Link>
       </li>
-      <li>
+      <li onClick={clickHandle}>
         <Link className="nav-links" to="/discouts">
           <span>الخصومات</span>
           <div>
@@ -19,12 +27,12 @@ const NavList = () => {
           </div>
         </Link>
       </li>
-      <li>
+      <li onClick={clickHandle}>
         <Link className="nav-links" to="/categories">
           الأقسام
         </Link>
       </li>
-      <li>
+      <li onClick={clickHandle}>
         <Link className="nav-links" to="/with-us">
           <span>انشر معنا</span>
           <div>
--- src/components/navbar/leftNav/register/Register.js
+++ src/components/navbar/leftNav/register/Register.js
@@ -1,12 +1,22 @@
 import { Link } from "react-router-dom";
 import "./Register.scss";
+import { useContext } from "react";
+import { MenuContext } from "../../../../hooks/contexts/useMenuContext";
 
 const Register = () => {
-    return (
-        <div className="register">
-            <Link className="register-link" to="/register">تسجيل</Link>
-        </div>
-    );
-}
+  const { setMenu } = useContext(MenuContext);
+
+  const clickHandle = () => {
+    setMenu(null);
+  };
+
+  return (
+    <div className="register" onClick={clickHandle}>
+      <Link className="register-link" to="/register">
+        تسجيل
+      </Link>
+    </div>
+  );
+};
 
 export default Register;
