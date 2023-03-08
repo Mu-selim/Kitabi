2095f06714f00bc521f26286106ae0dc5332c686
Muhammad Selim <muhammadselim@duck.com>
2023-03-08 17:27:04 +0200

    fix svg bugs

--- src/utils/icons/searchicon/SearchIcon.js
+++ src/utils/icons/searchicon/SearchIcon.js
@@ -12,9 +12,9 @@ const SearchIcon = () => {
     <svg
       fill={styleObject.fill}
       stroke={styleObject.stroke}
-      stroke-linecap={styleObject.strokeLinecap}
-      stroke-linejoin={styleObject.strokeLinejoin}
-      stroke-width={styleObject.strokeWidth}
+      strokeLinecap={styleObject.strokeLinecap}
+      strokeLinejoin={styleObject.strokeLinejoin}
+      strokeWidth={styleObject.strokeWidth}
       viewBox={styleObject.viewBox}
     >
       <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
