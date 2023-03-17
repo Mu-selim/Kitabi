5297e39f22a8f18b84d27881c31a97c8a54e1d55
Muhammad Selim <muhammadselim@duck.com>
2023-03-17 17:03:52 +0200

    Bottom Footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,10 +1,12 @@
 import './Footer.scss';
+import BottomFooter from './bottomFooter/BottomFooter';
 import TopFooter from './topFooter/TopFooter';
 
 const Footer = () => {
     return (
         <footer className="footer">
             <TopFooter />
+            <BottomFooter />
         </footer>
     );
 }
---
+++ src/components/footer/bottomFooter/BottomFooter.js
@@ -0,0 +1,19 @@
+import './BottomFooter.scss';
+
+const BottomFooter = () => {
+    const copyRightsText = 'جميع الحقوق محفوظة لكتابي 2023 ©';
+    const buildBy = {
+        name: 'Muhammad Selim',
+        link: 'https://www.linkedin.com/in/selimjs',
+        text: 'بواسطة'
+    }
+
+    return (
+        <div className="bottom-footer">
+            <p className="bottom-footer__text">{copyRightsText}</p>
+            <p className="bottom-footer__text">{buildBy.text} <a href={buildBy.link} target="_blank" rel="noreferrer">{buildBy.name}</a></p>
+        </div>
+    );
+}
+ 
+export default BottomFooter;
